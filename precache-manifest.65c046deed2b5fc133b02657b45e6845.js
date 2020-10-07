0c75cb433d22825400800347f7fecf4fa7f262b1
squashetonics <70538586+squashetonics@users.noreply.github.com>
2020-10-07 14:58:41 +0200

    Updates

---
+++ precache-manifest.65c046deed2b5fc133b02657b45e6845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8e3555508111fcfd5819a23e666a7a",
+    "url": "/drum/index.html"
+  },
+  {
+    "revision": "8852e8f26d90ca33f872",
+    "url": "/drum/static/css/main.7cf2636d.chunk.css"
+  },
+  {
+    "revision": "5d1d5edeb3464b122e75",
+    "url": "/drum/static/js/2.949cab73.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/drum/static/js/2.949cab73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8852e8f26d90ca33f872",
+    "url": "/drum/static/js/main.026531b6.chunk.js"
+  },
+  {
+    "revision": "f2ff93c4ee7ecb75a9cd",
+    "url": "/drum/static/js/runtime-main.0f22011c.js"
+  }
+]);
